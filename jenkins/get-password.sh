778c399cf80db422c909bc27426b633df6ced2d9
Taylor Kemper <tjkemper6@gmail.com>
2017-02-18 14:21:48 -0700

    modify jenkins config

--- jenkins/get-password.sh
+++ jenkins/get-password.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#printf '%x' $( echo '[65 69 55 119 70 49 52 82 76 103]' | awk '{print substr($0, 2, length($0) - 2)}' ) | xxd -r -p
+#printf '%x' $( echo '[65 66 67 68 69]' | awk '{print substr($0, 2, length($0) - 2)}' ) | xxd -r -p
 
 printf '%x' $( kubectl get secret --namespace default jenkins-jenkins -o jsonpath="{.data.jenkins-admin-password}" | awk '{print substr($0, 2, length($0) - 2)}' ) \
 | xxd -r -p
